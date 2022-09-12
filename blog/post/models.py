445cfbc4cfa64031b72365113bfea6ca854ce45d
Everton-Fontes <solucoestge@gmail.com>
2022-09-12 13:32:26 -0300

    apps creations

---
+++ blog/post/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from category.models import Category
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    content = models.TextField()
+    excerpt = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.DO_NOTHING,
+        blank=True
+    )
+    image_title = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to='fotos/%Y/%m/'
+    )
+    published = models.BooleanField(default=True)
